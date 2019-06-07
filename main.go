78754a166be498be237b3968e3dc677f4c85d0a8
Shohei Kamimori <jyane.kstm@gmail.com>
2019-06-07 21:59:33 +0900

    add tokenizer

--- main.go
+++ main.go
@@ -6,16 +6,83 @@ import (
 	"strconv"
 )
 
+type Type int
+
+const (
+	Eof = iota
+	Plus
+	Minus
+	Number
+)
+
+type Token struct {
+	typ   Type
+	value int
+}
+
+func isNumber(r rune) bool {
+	c := int(r) - '0'
+	return 0 <= c && c <= 9
+}
+
+func tokenize(s string, index int) []Token {
+	var res = []Token{}
+	r := []rune(s)[index]
+	N := len(s)
+	if r == '+' {
+		res = append(res, Token{Plus, 0})
+	} else if r == '-' {
+		res = append(res, Token{Minus, 0})
+	} else if isNumber(r) {
+		var ns string
+		for i := index; i < N; i++ {
+			if isNumber(rune(s[i])) {
+				ns += string(s[i])
+			} else {
+				break
+			}
+		}
+		num, err := strconv.Atoi(ns)
+		if err != nil {
+			panic("tokenize error: [" + ns + "]")
+		}
+		res = append(res, Token{Number, num})
+	}
+	if index < N-1 {
+		res = append(res, tokenize(s, index+1)...)
+	}
+	return res
+}
+
 func main() {
 	flag.Parse()
 	args := flag.Args()
-	num, err := strconv.Atoi(args[0])
-	if err != nil {
-		panic("error")
+	if len(args) < 1 {
+		fmt.Println("Usage: ")
+		panic("too few argments")
 	}
+
+	tokens := tokenize(args[0], 0)
+
 	fmt.Printf(".intel_syntax noprefix\n")
 	fmt.Printf(".global _main\n")
 	fmt.Printf("_main:\n")
-	fmt.Printf("  mov rax, %d\n", num)
+	fmt.Printf("  mov rax, %d\n", tokens[0].value)
+	i := 1
+	N := len(tokens)
+	for i < N {
+		if tokens[i].typ == Plus {
+			i++
+			if tokens[i].typ == Number {
+				fmt.Printf("  add rax, %d\n", tokens[i].value)
+			}
+		} else if tokens[i].typ == Minus {
+			i++
+			if tokens[i].typ == Number {
+				fmt.Printf("  sub rax, %d\n", tokens[i].value)
+			}
+		}
+		i++
+	}
 	fmt.Printf("  ret\n")
 }
--- test.sh
+++ test.sh
@@ -1,19 +1,21 @@
-#!/bin/bash
+#!/bin/bash -x
 
 assert() {
     expected="$1"
     input="$2"
-    go run main.go $2 > out.s
+    go run main.go "$input" > out.s
+    cat out.s
     gcc -o out out.s
     ./out
     actual="$?"
     if [ "$actual" != "$expected" ]; then
-        echo "$input := $actual != $expected"
+        echo "$input => $actual != $expected"
         exit 1
     fi
 }
 
 assert 42 42
 assert 0 0
+assert 9 ' 11 + 1 - 3 '
 
 echo 'Done'
